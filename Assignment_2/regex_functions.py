f9736e6acb92f5f8aa50fa58063afd27256531c4
Fung Joshua See Yui <c5fungjo@cdf.toronto.edu>
2015-07-28 18:21:22 -0400

    The inital code provided from the course

---
+++ Assignment_2/regex_functions.py
@@ -0,0 +1,27 @@
+"""
+# Copyright Nick Cheng, Brian Harrington, Danny Heap, 2013, 2014, 2015
+# Distributed under the terms of the GNU General Public License.
+#
+# This file is part of Assignment 2, CSCA48, Winter 2015
+#
+# This is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This file is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this file.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+# Do not change this import statement, or add any of your own!
+from regextree import RegexTree, StarTree, DotTree, BarTree, Leaf
+
+# Do not change anything above this comment except for the copyright
+# statement
+
+# Student code below this comment.
